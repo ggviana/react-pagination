20bed2c345cfb61ea59ae0f3c86ee529ec8e9f30
Guilherme Viana <ggviana@hotmail.com.br>
2016-09-11 22:18:31 -0300

    Adding a development env

---
+++ src/example/index.jsx
@@ -0,0 +1,5 @@
+import React      from "react"
+import ReactDOM   from "react-dom"
+import Pagination from "components/Pagination"
+
+ReactDOM.render(<Pagination />, document.getElementById("root"))
